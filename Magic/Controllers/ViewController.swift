526b8bf5787e024e7f9a34767acf319ca61d3379
GArifullinII <ildaridrisovich@gmail.com>
2023-01-18 18:23:27 +0400

    feat: added magicButton and action

--- Magic/Controllers/ViewController.swift
+++ Magic/Controllers/ViewController.swift
@@ -13,11 +13,31 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "Ask Me Anything..."
         label.textColor = .white
-        label.font = label.font.withSize(40)
+        label.font = label.font.withSize(45)
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
+    }()
+    
+    private let magicUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "ball1")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let magicButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Ask", for: .normal)
+        button.titleLabel?.font = button.titleLabel?.font.withSize(45)
+        button.setTitleColor(#colorLiteral(red: 0.3828315139, green: 0.8034424186, blue: 0.9958037734, alpha: 1), for: .normal)
+        button.backgroundColor = .white
+        button.addTarget(self, action: #selector(magicButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
     }()
 
     override func viewDidLoad() {
@@ -29,8 +49,14 @@ class ViewController: UIViewController {
         setConstraints()
     }
     
+    @objc private func magicButtonTapped() {
+        print("Hello")
+    }
+    
     private func setupViews() {
         view.addSubview(magicLabel)
+        view.addSubview(magicUIImageView)
+        view.addSubview(magicButton)
     }
 }
 
@@ -41,6 +67,19 @@ extension ViewController {
             magicLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             magicLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
             magicLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+        
+        NSLayoutConstraint.activate([
+            magicUIImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            magicUIImageView.topAnchor.constraint(equalTo: magicLabel.bottomAnchor, constant: 50),
+            magicUIImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7)
+        ])
+        
+        NSLayoutConstraint.activate([
+            magicButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            magicButton.topAnchor.constraint(equalTo: magicUIImageView.bottomAnchor, constant: 50),
+            magicButton.widthAnchor.constraint(equalToConstant: 150),
+            magicButton.heightAnchor.constraint(equalToConstant: 60)
         ])
     }
 }
