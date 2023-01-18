7abd06bd31342271cbcd3332ed6f7792270aacf6
GArifullinII <ildaridrisovich@gmail.com>
2023-01-18 17:40:56 +0400

    feat: added magicLabel

--- Magic/Controllers/ViewController.swift
+++ Magic/Controllers/ViewController.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let magicLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Ask Me Anything..."
+        label.textColor = .white
+        label.font = label.font.withSize(40)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = #colorLiteral(red: 0.3828315139, green: 0.8034424186, blue: 0.9958037734, alpha: 1)
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    private func setupViews() {
+        view.addSubview(magicLabel)
     }
 }
 
+extension ViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            magicLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            magicLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            magicLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            magicLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+    }
+}
